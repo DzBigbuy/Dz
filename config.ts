f64814d233472feea4d381abfaff80fce73aec60
DzBigbuy <boukhezartarek@gmail.com>
2026-01-02 01:32:25 +0100

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  "projectId": "studio-7316459997-f5ae3",
+  "appId": "1:647609073070:web:d17c6eee6a15eb42a45c3f",
+  "apiKey": "AIzaSyBR4q9dem2cVUY-r7bSwzsLQV4M2LNi4zQ",
+  "authDomain": "studio-7316459997-f5ae3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "647609073070"
+};
+
+    
